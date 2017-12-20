15adda488dc8f3da6fc682416bf96eb7a15a3bf5
Dewitt.Max <Dewitt.Max@SEMS-259-09.SEMS259.LAB>
2017-12-20 14:36:10 -0800

    holiday tree

--- src/ChristmasTree.java
+++ src/ChristmasTree.java
@@ -12,6 +12,7 @@ public class ChristmasTree {
 
 	Robot rob  = new Robot();
 	
+	
     public static void main(String[] args) {
         ChristmasTree ohChristmasTree = new ChristmasTree();
         ohChristmasTree.drawStar();
@@ -25,56 +26,73 @@ public class ChristmasTree {
 
 
     void drawTreeBody() {
-        // 8. Change the color of the line the robot draws to forest green    
-
-
+        // 8. Change the color of the line the robot draws to forest green   
+    	rob.miniaturize();
+        rob.penDown();
+        rob.setPenColor(0, 153, 0);
+        rob.turn(90);
         // 1. Make a variable for turnAmount and set it to 175
-
+       int turnAmount = 175;
 
         // 2. Start the Robot facing to the right
-
+       
 
         // 5. Repeat steps 3 through 11, 11 times
 
-
+            for (int i = 0; i < 17; i++) {
+				
+			
             // 3. Move the robot the width of the tree
-    
+            rob.move((int) treeWidth);
             // 4. Turn the robot the current turnAmount to the right
-    
+            rob.turn(turnAmount);
             // 6. Set the treeWidth to the current treeWidth times the scale
-    
+            treeWidth = (treeWidth * scale);
             // 7. Move the robot the width of a tree again
-    
+            rob.move((int)treeWidth);
             // 9. Turn the robot the current turn amount to the LEFT
-    
+            rob.turn(-turnAmount);
             // 10. Set the treeWidth to the current treeWidth times the scale again
-    
+            treeWidth = (treeWidth * scale);
             // 11. Decrease turnAmount by 1
-
-
+            turnAmount = (turnAmount - 1);
+            }
 }
     
     void drawTreeTrunk() {
         // 1. Move the robot half the width of the tree
-
+        rob.move((int)treeWidth / 2);
 
         // 2. Change the robot so that it is pointing straight down
-
+        rob.turn(90);
 
         // 4. Set the pen width to the tree width divided by 10
-
+        rob.setPenWidth((int)treeWidth / 10);
 
         // 5. Change the color of the line the robot draws to brown
-
+        rob.setPenColor(102, 51, 0);
 
         // 3. Move the robot a quarter the tree width
-
+        rob.move((int)treeWidth / 4);
 
     }
     
     void drawStar() {
         // * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a star.
-    
+        rob.moveTo(500, 60);
+    	
+       rob.setSpeed(100);
+       rob.penDown();
+       rob.setPenWidth(100);
+       rob.setPenColor(200, 0, 0);
+       for (int s = 0; s < 10 ; s++) {
+    	   rob.move(50);
+    	   rob.turn(144);
+          
+		
+	}
+       
+       
     }
 
 
